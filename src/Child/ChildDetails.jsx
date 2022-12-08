cd6cb454d7fc3576b112a300faf977cadc8d8ca9
Umed Jadhav <umed.jadhav@phonepe.com>
2022-12-09 04:32:25 +0530

    Generalized the child pages to setup child and parent pages

--- src/Child/ChildDetails.jsx
+++ src/Child/ChildDetails.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import BasicUserInformation from './BasicUserInformation';
-import RecentTx from './RecentTx';
 
 
 export default class ChildDetails extends Component {
@@ -9,13 +8,6 @@ export default class ChildDetails extends Component {
     }
     render() {
         const userInfo = this.props.userInfo
-        // const userInfo = {
-        //     userName: "Hunter biden",
-        //     approver: "Joe Biden",
-        //     createdAt: "10-12-2022",
-        //     perTxLimit: "100",
-        //     monthlyTxLimit: "2000"
-        // }
         return <>
             <div className='child_details_container'>
                 <h2 style={{"margin-bottom": "25px", "margin-left": "75px", "padding": "10px"}}>Child Details</h2>
