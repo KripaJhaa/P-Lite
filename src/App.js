86d3821a6a167aaff27dce0bdf768655b51b0d62
Umed Jadhav <umed.jadhav@phonepe.com>
2022-12-08 16:57:20 +0530

    Added basic router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/users" element={<Users/>}/>
+          <Route exact path="/" element={<Home/>}/>
+        </Routes>
+    </Router>
   );
 }
+
+function About(props) {
+    return "About page"
+}
+
+function Users(props) {
+    return "Users page"
+}
+function Home(props){
+    return "Home page"
+}
+
 
 export default App;
